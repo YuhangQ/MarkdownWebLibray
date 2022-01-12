27a1115548f21205e4b406c47279bffadb6d76fa
YuhangQ <i@yuhangq.com>
2022-01-12 20:17:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-main>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+html, body, #app {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: calc(100vh - 120px);
+}
+
+</style>
